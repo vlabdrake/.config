ced77d818d0e5822caa638df5f5f8029154c9584
Vladimir Abdrakhmanov <vlabdrake@protonmail.com>
2023-03-08 20:00:36 +0300

    add neovim

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,31 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+     requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-lualine/lualine.nvim'
+
+  use 'neovim/nvim-lspconfig'
+
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+  }
+
+  use { 'itsjunetime/rose-pine-vim' }
+end)
+
+
