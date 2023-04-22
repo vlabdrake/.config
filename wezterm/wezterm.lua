c3c0d02f7ff745ece4d49bb2100a3d5c1a019751
Vladimir Abdrakhmanov <vlabdrake@protonmail.com>
2023-04-22 23:48:50 +0300

    wezterm: selection colors and integrated titlebar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,17 +13,23 @@ return {
   font = wezterm.font 'JetBrains Mono',
   font_size = 13,
   default_prog = { '/usr/bin/fish', '-l' },
-  window_decorations = "RESIZE",
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  integrated_title_button_style = "Gnome",
   window_frame = {
-  border_left_width = '1px',
-  border_right_width = '1px',
-  border_bottom_height = '1px',
-  border_top_height = '1px',
-  border_left_color = 'black',
-  border_right_color = 'black',
-  border_bottom_color = 'black',
-  border_top_color = 'black',
-},
-  hide_tab_bar_if_only_one_tab = true
+    border_left_width = '1px',
+    border_right_width = '1px',
+    border_bottom_height = '1px',
+    border_top_height = '1px',
+    border_left_color = 'black',
+    border_right_color = 'black',
+    border_bottom_color = 'black',
+    border_top_color = 'black',
+  },
+  colors = {
+    -- the foreground color of selected text
+    selection_fg = 'white',
+    -- the background color of selected text
+    selection_bg = '#7c4dff',
+  },
 }
 
